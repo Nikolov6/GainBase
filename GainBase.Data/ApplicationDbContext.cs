02becadceec54066a8dd476e5c5cb3dcc7ceb7ad
Nikolov6 <nklnklv1@gmail.com>
2026-03-11 22:44:53 +0200

    add db sets and override savechanges to set current time

--- GainBase.Data/ApplicationDbContext.cs
+++ GainBase.Data/ApplicationDbContext.cs
@@ -1,13 +1,59 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using GainBase.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
-namespace GainBase.Web.Data
+namespace GainBase.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public virtual DbSet<Exercise> Exercises { get; set; } = null!;
+        public virtual DbSet<MuscleGroup> MuscleGroups { get; set; } = null!;
+        public virtual DbSet<Equipment> Equipment { get; set; } = null!;
+        public virtual DbSet<UserExercise> UsersExercises { get; set; } = null!;
+
+        
+        public override int SaveChanges()
+        {
+            ApplyTimestamps();
+            return base.SaveChanges();
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            ApplyTimestamps();
+            return await base.SaveChangesAsync(cancellationToken);
+        }    
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+
+        private void ApplyTimestamps()
+        {
+            foreach (EntityEntry<Exercise> entry in ChangeTracker.Entries<Exercise>())
+            {
+                if (entry.State == EntityState.Added)
+                    entry.Entity.CreatedAt = DateTime.UtcNow;
+
+                if (entry.State == EntityState.Modified)
+                    entry.Entity.UpdatedAt = DateTime.UtcNow;
+            }
+
+            foreach (EntityEntry<UserExercise> entry in ChangeTracker.Entries<UserExercise>())
+            {
+                if (entry.State == EntityState.Added)
+                    entry.Entity.SavedAt = DateTime.UtcNow;
+            }
         }
     }
 }
